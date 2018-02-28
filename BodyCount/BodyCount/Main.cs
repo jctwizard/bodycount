4e41b535b9795c67add01bf8b1fd8adaae2fcc0d
jctwood <jamie.ct.wood@gmail.com>
2018-02-28 16:58:00 +0000

    Add project files

---
+++ BodyCount/BodyCount/Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GTA;
+using GTA.Native;
+using GTA.Math;
+
+
+namespace BodyCount
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            Tick += Update;
+            KeyDown += Input;
+
+            Start();
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update(object sender, EventArgs e)
+        {
+
+        }
+
+        void Input(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.K)
+            {
+                Ped ped = World.CreateRandomPed(Game.Player.Character.Position.Around(2.0f));
+
+                if (ped.Exists() == false)
+                {
+                    UI.Notify("Ped not spawned");
+                }
+
+                UI.ShowSubtitle("Body Count: " + World.GetAllPeds().Count().ToString());
+            }
+        }
+    }
+}
