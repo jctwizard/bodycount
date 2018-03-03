ef49e1a31835a494fde347ea4663243ed8f7743c
jctwood <jamie.ct.wood@gmail.com>
2018-03-03 12:20:05 +0000

    Add old script

---
+++ BodyCount/BodyCount/BodyCountV.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+
+namespace BodyCountV
+{
+    public class BodyCountV : Script
+    {
+        int bodyCount = 0;
+
+        public BodyCountV()
+        {
+            UI.Notify("Hello World");
+
+            Tick += Update;
+            KeyDown += Input;
+            Interval = 1;
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Input(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.K)
+            {
+                Ped newPed = World.CreateRandomPed(Game.Player.Character.Position.Around(1.0f));
+                newPed.CanRagdoll = true;
+                newPed.Kill();
+                newPed.ApplyForce(Vector3.WorldUp * 1.0f);
+
+                bodyCount++;
+                UI.Notify(bodyCount.ToString());
+            }
+            if (e.KeyCode == Keys.C)
+            {
+                UI.Notify(World.GetAllPeds().Count().ToString());
+            }
+        }
+    }
+}
